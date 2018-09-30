a615b75844ad2d1b055f30f10481139ff867eaeb
shcho_coworksys <shcho@coworksys.com>
2018-10-01 00:27:02 +0900

    create NavButton

--- routes/sharedRoutes.js
+++ routes/sharedRoutes.js
@@ -1,21 +1,24 @@
+import React from "react";
 import LikesScreen from "../screens/LikesScreen";
 import CommentsScreen from "../screens/CommentsScreen";
+import NavButton from "../components/NavButton";
 
 const sharedRoutes = {
-    Likes: {
-        screen: LikesScreen
-    },
-    Comments: {
-        screen: CommentsScreen
-    }
+  Likes: {
+    screen: LikesScreen
+  },
+  Comments: {
+    screen: CommentsScreen
+  }
 };
 
 const sharedOptions = {
-    navigationObtions: {
-        headerStyle: {
-            backgroundColor: "#FBFBFB"
-        }
+  navigationOptions: {
+    headerLeft: props => <NavButton iconName={"ios-arrow-back"} {...props} />,
+    headerStyle: {
+      backgroundColor: "#FBFBFB"
     }
+  }
 };
 
 export { sharedOptions };
