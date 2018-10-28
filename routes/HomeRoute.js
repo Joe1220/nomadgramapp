05f16ba1d75ba73196062e47945d5c8fd2d59549
joe1220 <joe1220@daum.net>
2018-10-28 20:56:32 +0900

    Creating the Feed Header

--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -1,17 +1,25 @@
-import { createStackNavigator } from "react-navigation";
-import FeedScreen from "../screens/FeedScreen";
-import sharedRoutes, { sharedOptions } from "./sharedRoutes";
+import React from "react";
+import PropTypes from "prop-types";
+import { TouchableWithoutFeedback, View, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-const HomeRoutes = createStackNavigator(
-    {
-        Home: {
-            screen: FeedScreen
-        },
-        ...sharedRoutes
-    },
-    {
-        ...sharedOptions
-    }
-)
+const NavButton = props => (
+  <TouchableWithoutFeedback onPressOut={props.onPress}>
+    <View style={styles.container}>
+      <Ionicons name={props.iconName} color={"black"} size={30} />
+    </View>
+  </TouchableWithoutFeedback>
+);
 
-export default HomeRoutes
+NavButton.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  iconName: PropTypes.string.isRequired
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10
+  }
+});
+
+export default NavButton;
