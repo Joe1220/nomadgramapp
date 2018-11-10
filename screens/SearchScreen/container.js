f834931733332b9b22ecb72da8ba4c5f7ef2d7b9
joe1220 <joe1220@daum.net>
2018-11-10 18:27:07 +0900

    NotificationsScreen part One

--- screens/SearchScreen/container.js
+++ screens/SearchScreen/container.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import SearchScreen from "./presenter";
@@ -12,7 +11,7 @@ class Container extends Component {
     };
   };
   static propTypes = {
-    getEmptyFeed: PropTypes.func.isRequired,
+    getEmptySearch: PropTypes.func.isRequired,
     searchHashtag: PropTypes.func.isRequired,
     search: PropTypes.array
   };
@@ -41,18 +40,22 @@ class Container extends Component {
   }
   _submitSearch = text => {
     const { searchingBy } = this.state;
-    const { searchHashtag } = this.props;
+    const { searchHashtag, getEmptySearch } = this.props;
+    if (text === "") {
+      getEmptySearch();
+    } else {
+      searchHashtag(text);
+    }
     this.setState({
       searchingBy: text,
       isFetching: true
     });
-    searchHashtag(text);
   };
   _refresh = () => {
     const { searchingBy } = this.state;
-    const { getEmptyFeed, searchHashtag } = this.props;
+    const { getEmptySearch, searchHashtag } = this.props;
     if (searchingBy === "") {
-      getEmptyFeed();
+      getEmptySearch();
     } else {
       searchHashtag(searchingBy);
     }
